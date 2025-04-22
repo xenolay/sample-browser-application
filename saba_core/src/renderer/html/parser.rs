5ab7469c203dec25d5d6e322814964a107916557
Yosuke Enokida <p.ocean.enokida@gmail.com>
2025-04-22 17:59:12 +0900

    feat: implement skeleton of construct_tree

--- saba_core/src/renderer/html/parser.rs
+++ saba_core/src/renderer/html/parser.rs
@@ -26,3 +26,27 @@ pub enum InsertionMode {
     AfterBody,
     AfterAfterBody,
 }
+
+impl HtmlParser {
+    pub fn new(tokenizer: HtmlTokenizer) -> Self {
+        Self { window: Window::new(), current_mode: InsertionMode::Initial, original_mode: InsertionMode::Initial, stack_of_open_elements: Vec::new(), tokenizer }
+    }
+
+    pub fn construct_tree(&mut self) -> Window {
+        let token = self.tokenizer.next();
+        while token.is_some() {
+            match self.current_mode {
+                InsertionMode::Initial => todo!(),
+                InsertionMode::BeforeHtml => todo!(),
+                InsertionMode::BeforeHead => todo!(),
+                InsertionMode::InHead => todo!(),
+                InsertionMode::AfterHead => todo!(),
+                InsertionMode::InBody => todo!(),
+                InsertionMode::Text => todo!(),
+                InsertionMode::AfterBody => todo!(),
+                InsertionMode::AfterAfterBody => todo!(),
+            }    
+        }
+        self.window.clone()
+    }
+}
