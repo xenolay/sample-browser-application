38863c5d4a6d4a35512b7e56b7c8c9a28f087b8c
Yosuke Enokida <p.ocean.enokida@gmail.com>
2025-04-30 11:58:30 +0900

    feat: implement easy part of Tokenizer

--- saba_core/src/renderer/css/token.rs
+++ saba_core/src/renderer/css/token.rs
@@ -1,4 +1,4 @@
-use alloc::string::String;
+use alloc::{string::String, vec::Vec};
 
 
 #[derive(Debug, Clone, PartialEq)]
@@ -15,4 +15,52 @@ pub enum CssToken {
     Ident(String),
     StringToken(String),
     AtKeyword(String),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct CssTokenizer {
+    pos: usize,
+    input: Vec<char>
+}
+
+impl CssTokenizer {
+    pub fn new(css: String) -> Self {
+        Self { pos: 0, input: css.chars().collect() }
+    }
+}
+
+impl Iterator for CssTokenizer {
+    type Item = CssToken;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if self.pos > self.input.len() {
+                return None;
+            }
+
+            let c = self.input.get(self.pos)?;
+
+
+            let token = match c {
+                '(' => CssToken::OpenParenthesis,
+                ')' => CssToken::CloseParenthesis,
+                ',' => CssToken::Delim(','),
+                '.' => CssToken::Delim('.'),
+                ':' => CssToken::Colon,
+                ';' => CssToken::Semicolon,
+                '{' => CssToken::OpenCurly,
+                '}' => CssToken::CloseCurly,
+                ' ' | '\n' => {
+                    self.pos += 1;
+                    continue;
+                }
+                _ => {
+                    unimplemented!("char {} is not supported yet", c)
+                }
+            };
+
+            self.pos += 1;
+            return Some(token);
+        }
+    }
 }
