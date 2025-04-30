2a5cce593cb098489ff80c79ad23dd7f9de360c8
Yosuke Enokida <p.ocean.enokida@gmail.com>
2025-04-30 11:40:43 +0900

    feat: enumerate css tokens

---
+++ saba_core/src/renderer/css/token.rs
@@ -0,0 +1,18 @@
+use alloc::string::String;
+
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum CssToken {
+    HashToken(String),
+    Delim(char),
+    Number(f64),
+    Colon,
+    Semicolon,
+    OpenParenthesis,
+    CloseParenthesis,
+    OpenCurly,
+    CloseCurly,
+    Ident(String),
+    StringToken(String),
+    AtKeyword(String),
+}
