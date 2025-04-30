e6bcf5bb2dd4372fa95b4d2057a62dc97d96a356
Yosuke Enokida <p.ocean.enokida@gmail.com>
2025-04-30 18:08:48 +0900

    feat: implement structs for cssom

--- saba_core/src/renderer/css/cssom.rs
+++ saba_core/src/renderer/css/cssom.rs
@@ -1,6 +1,8 @@
 use core::iter::Peekable;
 
-use super::token::CssTokenizer;
+use alloc::{string::String, vec::Vec};
+
+use super::token::{CssToken, CssTokenizer};
 
 #[derive(Debug, Clone)]
 pub struct CssParser {
@@ -10,5 +12,64 @@ pub struct CssParser {
 impl CssParser {
     pub fn new(tokenizer: CssTokenizer) -> Self {
         Self { tokenizer: tokenizer.peekable() }
+    }
+}
+
+pub struct StyleSheet {
+    pub rules: Vec<QualifiedRule>,
+}
+
+impl StyleSheet {
+    pub fn new() -> Self {
+        Self { rules: Vec::new() }
+    }
+
+    pub fn set_rules(&mut self, rules: Vec<QualifiedRule>) {
+        self.rules = rules;
+    }
+}
+
+pub struct QualifiedRule {
+    pub selector: Selector,
+    pub declarations: Vec<Declaration>,
+}
+
+impl QualifiedRule {
+    pub fn new(selector: Selector, declarations: Vec<Declaration>) -> Self {
+        Self { selector, declarations }
+    }
+
+    pub fn set_selector(&mut self, selector: Selector) {
+        self.selector = selector;
+    }
+
+    pub fn set_declarations(&mut self, declarations: Vec<Declaration>) {
+        self.declarations = declarations;
+    }
+}
+
+pub enum Selector {
+    TypeSelector(String),
+    ClassSelector(String),
+    IdSelector(String),
+    UnknownSelector,
+}
+
+pub struct Declaration {
+    pub property: String,
+    pub value: CssToken,
+}
+
+impl Declaration {
+    pub fn new() -> Self {
+        Self { property: String::new(), value: CssToken::Ident(String::new()) }
+    }
+
+    pub fn set_property(&mut self, property: String) {
+        self.property = property;
+    }
+
+    pub fn set_value(&mut self, value: CssToken) {
+        self.value = value;
     }
 }
